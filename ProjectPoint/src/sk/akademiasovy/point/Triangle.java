5d2e50623c7e42a90258778742952cd4dfff3d28
Daniela8888 <32717667+Daniela8888@users.noreply.github.com>
2018-01-26 12:38:08 +0100

    Add files via upload

---
+++ ProjectPoint/src/sk/akademiasovy/point/Triangle.java
@@ -0,0 +1,66 @@
+package sk.akademiasovy.point;
+
+public class Triangle implements InterfaceTriangle{
+    private Point a;
+    private Point b;
+    private Point c;
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getSideA(){
+        return b.getDistanceFromOtherPoint(c);
+
+    }
+
+    public double getSideB(){
+        return a.getDistanceFromOtherPoint(c);
+    }
+
+    public double getSideC(){
+        return b.getDistanceFromOtherPoint(a);
+    }
+    public double getPerimeter(){
+        return getSideA()+getSideB()+getSideC();
+    }
+    public double getArea(){
+        double s;
+        s=getPerimeter()/2;
+        return Math.sqrt(s*(s-getSideA())*(s-getSideB())*(s-getSideC()));
+    }
+
+    @Override
+    public boolean isRectangular() {
+        if(getSideA()*getSideA()+getSideB()*getSideB()==getSideC()*getSideC())
+            return true;
+        else if(getSideA()*getSideA()+getSideC()*getSideC()==getSideB()*getSideB())
+            return true;
+        else if(getSideB()*getSideB()+getSideC()*getSideC()==getSideA()*getSideA())
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public boolean isEquilateral() {
+        if(getSideA()+getSideB()==getSideC()/2)
+            return true;
+        else
+            return false;
+    }
+
+    @Override
+    public boolean isIsosceles() {
+        if(getSideA()==getSideB())
+            return true;
+        else if(getSideA()==getSideC())
+            return true;
+        else if(getSideB()==getSideC())
+            return true;
+        else
+            return false;
+    }
+}
